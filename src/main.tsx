0779b5d46de3235be538d4edebf080772e36d5c2
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-05-05 10:57:16 +0100

    chore:  App configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { NextUIProvider } from '@nextui-org/react';
+
+import { RouterProvider } from 'react-router-dom';
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+import { Toaster } from 'react-hot-toast';
+
+import { Router } from './router';
+
+import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <Toaster />
+        <RouterProvider router={Router} />
+      </NextUIProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
