4d4ade089eed34873717fdc23c56478207cf0d76
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-05-05 10:51:06 +0100

    config: added nextui plugin to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const { nextui } = require('@nextui-org/react');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stanbic: '#0062E1',
+      },
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  darkMode: 'false',
+  plugins: [nextui()],
 };
