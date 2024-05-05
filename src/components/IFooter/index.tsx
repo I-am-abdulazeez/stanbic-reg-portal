26d4ba4daaba9790ca6d93c49617b2838acd92d5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-05-05 11:00:06 +0100

    chhore: Footer component

---
+++ src/components/IFooter/index.tsx
@@ -0,0 +1,9 @@
+export default function IFooter() {
+  return (
+    <div className="font-inter bg-gray-100 p-2 text-center">
+      <p className="text-sm">
+        Copyright Onboarding | StanbicIBTC Pensions {new Date().getFullYear()}
+      </p>
+    </div>
+  );
+}
