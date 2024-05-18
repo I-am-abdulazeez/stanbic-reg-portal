82582dcf285f2602dcaa447046b55ee9c3d3371c
AbdulAzeez <abdulazeezolamide50@gmail.com>
2024-05-18 01:50:30 +0100

    chore: fetchTempCustomerCountries, fetchTempCustomerStates, fetchTempCustomerLocalGovt, fetchTempCustomerByDetails, fetchTempCustomerImage, updateTempCustomerImage.
    sendCustomerToPencom: WIP (not done)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,11 @@
 import { TEMPCUST_API, TEMPIMAGE_API } from 'src/data';
 
-import { formStepData } from 'src/types';
-
-// type PickContinue = Pick<RegFormType, 'email' | 'phoneNumber'>;
+import {
+  DocumentUploadType,
+  ImageIDs,
+  UserDetails,
+  formStepData,
+} from 'src/types';
 
 const fetchTempCustomers = () => {
   return fetch(TEMPCUST_API, {
@@ -12,10 +15,29 @@ const fetchTempCustomers = () => {
   }).then((response) => response.json());
 };
 
-const fetchTempCustomerByDetails = (data: {
-  email: string | undefined;
-  phoneNumber: string | undefined;
-}) => {
+const fetchTempCustomerCountries = () => {
+  return fetch(`${TEMPCUST_API}/Countries`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+const fetchTempCustomerStates = () => {
+  return fetch(`${TEMPCUST_API}/States`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+const fetchTempCustomerLocalGovt = (stateCode: string | undefined) => {
+  return fetch(`${TEMPCUST_API}/LocalGovt?stateCode=${stateCode}`, {
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+
+const fetchTempCustomerByDetails = (data: UserDetails) => {
   return fetch(
     `${TEMPCUST_API}/${data.email}?phoneNumber=${data.phoneNumber}`,
     {
@@ -25,6 +47,25 @@ const fetchTempCustomerByDetails = (data: {
       },
     }
   ).then((response) => response.json());
+};
+
+const fetchTempCustomerImage = (data: ImageIDs) => {
+  return fetch(`${TEMPIMAGE_API}/${data.id}/${data.customerID}`, {
+    headers: {
+      method: 'GET',
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+
+const sendCustomerToPencom = (no: number) => {
+  return fetch(`${TEMPCUST_API}/generate`, {
+    headers: {
+      method: 'POST',
+      'Content-type': 'application/json; charset=UTF-8',
+      body: JSON.stringify(no),
+    },
+  }).then((response) => response.json());
 };
 
 // https://jsonplaceholder.typicode.com/posts
@@ -53,6 +94,8 @@ const updateTempCustomerDetails = async (data: formStepData) => {
     }
   );
 
+  console.log(response.url);
+
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
@@ -60,7 +103,9 @@ const updateTempCustomerDetails = async (data: formStepData) => {
   return await response.json();
 };
 
-const createTempCustomerImage = async (data: any) => {
+const createTempCustomerImage = async (
+  data: Omit<DocumentUploadType, 'id' | 'customerID'>
+) => {
   const response = await fetch(TEMPIMAGE_API, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -74,10 +119,33 @@ const createTempCustomerImage = async (data: any) => {
   return await response.json();
 };
 
+const updateTempCustomerImage = async (data: ImageIDs) => {
+  const response = await fetch(
+    `${TEMPIMAGE_API}/${data?.customerID}?ID=${data?.id}`,
+    {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
+};
+
 export {
-  createTempCustomer,
   fetchTempCustomers,
-  updateTempCustomerDetails,
   fetchTempCustomerByDetails,
+  fetchTempCustomerImage,
+  fetchTempCustomerCountries,
+  fetchTempCustomerStates,
+  fetchTempCustomerLocalGovt,
+  sendCustomerToPencom,
+  createTempCustomer,
   createTempCustomerImage,
+  updateTempCustomerImage,
+  updateTempCustomerDetails,
 };
